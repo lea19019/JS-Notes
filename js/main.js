cfa524be09a55d05d6711c3d1b2b76b828d2d68d
Adrian Leal <lea19019@byui.edu>
2021-05-05 10:21:18 -0500

    'portfolio'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const links = [
   {
-    label: "Week 02 notes",
+    label: "Week 01 notes",
     url: "week1/index.html"
   },
   {
@@ -13,15 +13,14 @@ const links = [
   }
 ]
 
-var links_list = document.getElementByClassName('list-links');
+let itemsList =  document.querySelector('.list-link');
 
 for (let index = 0; index < links.length; index++) {
   let listItem = document.createElement('LI');
   let linkWeek = document.createElement('A');
-  linkWeek.setAttribute('src', links[index]['url']);
+  linkWeek.setAttribute('href', links[index]['url']);
   linkWeek.innerText = links[index]['label'];
   listItem.appendChild(linkWeek);
-  links_list.appendChild(listItem);
-  console.log(links_list);
-  
+
+  document.querySelector('.list-links').appendChild(listItem);
 }
