bf1147c2f1c059bba03a3e9006fb555220e80b94
Adrian Castillo <lea19019@byui.edu>
2021-07-20 14:13:26 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ if (document.querySelector('.menu-area')) {
     const navClassMenu = document.querySelector('.menu-area');
     navClassMenu.innerHTML = `
 <ul>
-    <li><a href="/index.html">JS Journey</a></li>
+    <li><a href="/JS-Journey/index.html">JS Journey</a></li>
     <li><a href="/TodoApp/index.html">To Do App</a></li>
     <li><a href="https://lea19019.github.io/CountryMania/">Country Mania</a></li>
 </ul>
