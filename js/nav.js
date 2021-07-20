8a6aec27215e151d942085089d1c06f5c6a09fda
Adrian Castillo <lea19019@byui.edu>
2021-07-20 14:13:55 -0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ if (document.querySelector('.menu-area')) {
     navClassMenu.innerHTML = `
 <ul>
     <li><a href="/JS-Journey/index.html">JS Journey</a></li>
-    <li><a href="/TodoApp/index.html">To Do App</a></li>
+    <li><a href="/JS-Journey/TodoApp/index.html">To Do App</a></li>
     <li><a href="https://lea19019.github.io/CountryMania/">Country Mania</a></li>
 </ul>
 `;
@@ -13,7 +13,7 @@ if (document.querySelector('.menu-area')) {
 <ul>
     <li><button class="homeView">Home</button></li>
     <li><button class="projectsView">Projects</button></li>
-    <li><a href="/TodoApp/index.html">To Do App</a></li>
+    <li><a href="/JS-Journey/TodoApp/index.html">To Do App</a></li>
     <li><a href="https://lea19019.github.io/CountryMania/">Country Mania</a></li>
 </ul>
 `;
